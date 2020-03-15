081cee0438b6ee7cb2a77839c7967116e3c841ab
d3xian <515224526@qq.com>
2020-03-16 00:30:05 +0800

    start set Login

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './login.less'
+
+// 登录组件
+class Login extends Component {
+  render() {
+    return (<div className="login">
+      <header className="login-header">header</header>
+      <section className="login-content">header</section>
+    </div>)
+  }
+}
+
+export default Login
