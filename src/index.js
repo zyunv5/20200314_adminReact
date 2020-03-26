9ece550ea52508282c418df5221390427e94416a
d3xian <515224526@qq.com>
2020-03-27 00:42:20 +0800

    redux  branch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 //读取local中保存的user，保存到内存中
-import memoryUtils from "./utils/memoryUtils";
-import storageUtils from "./utils/localStorageUtils";
-const user = storageUtils.getUser();
-memoryUtils.user = user;
+import memoryUtils from './utils/memoryUtils'
+import storageUtils from './utils/localStorageUtils'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux'
+import store from './redux/store'
+const user = storageUtils.getUser()
+memoryUtils.user = user
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
