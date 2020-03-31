9d8cdf7f913ffbf868b7c0238a0ffed815004b98
zyunv5 <515224526@qq.com>
2020-03-31 14:18:22 +0800

    fixed:部分替换使用hooks

--- README.md
+++ README.md
@@ -1,8 +1,53 @@
-
 # git 参考手册地址
+
 > http://gitref.justjavac.com/
 
 # 使用脚手架搭建项目
+
 create-react-app 搭建项目
 
 sudo create-react-app react-admin_client （创建项目
+
+# hooks
+
+## useState
+
+```js
+const [count,setCount]=useState(0)
+const [obj,setObj]=useState({name:"zhangsan"})
+const [arr,setArr]=useState([1,2,3])
+const [func,setFunc]=useState(()=>{return 1})
+<>
+  <h2>{count}</h2>
+  <button onClick={()=>{setCount(count+1)}}></button>
+
+  <h2>{obj}</h2>
+  <button onClick={()=>{setObj({...obj,name:'lisi'})}}></button>
+
+  <h2>{arr}</h2>
+  <button onClick={()=>{setArr(
+    ()=>{arr.push(4);return [...]}
+  )}}></button>
+
+  <h2>{func}</h2>
+</>
+```
+## useEffect
+1. 相当于 DidMount DidUpdate和WillUnmount
+2. 副作用（DOM操作、数据更新、组件更新）
+3. useEffect 可以获取props和state，采用闭包的形式
+4. 无阻塞更新
+5. useEffect（回调函数，数组（可不写））
+6. 多个useEffect
+```js
+const [count,setCount]=useState(0);
+useEffect(()=>{
+  console.log(count)
+})
+//不增加第二个参数的时候，监听所有数据改动
+
+useEffect(()=>{
+  console.log(count)
+},[count])
+//只监视当前的count状态
+```
--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Button, Table, Modal, message } from "antd";
 import LinkButton from "../../component/link-button";
 import { formateDate } from "../../utils/dateUtils";
@@ -7,74 +7,91 @@ import { reqUsers, reqDeleteUsers, reqAddOrUpdateUser } from "../../api";
 import storageUtils from "../../utils/localStorageUtils";
 import UserForm from "./useForm";
 
-class User extends Component {
-  state = {
-    users: [],
-    roles: [],
-    isShow: false
+function User() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [roles, setRoles] = useState([]);
+  const [isShow, setShow] = useState(false);
+  const [roleNames, setRoleNames] = useState();
+  const [columns] = useState([
+    {
+      title: "用户名",
+      dataIndex: "username"
+    },
+    {
+      title: "邮箱",
+      dataIndex: "email"
+    },
+    {
+      title: "电话",
+      dataIndex: "phone"
+    },
+    {
+      title: "创建时间",
+      dataIndex: "create_time",
+      render: create_time => formateDate(create_time)
+    },
+    {
+      title: "所属角色",
+      dataIndex: "role_id",
+      render: role_id => {
+        if (role_id) {
+          console.log(roleNames,role_id)
+          return roleNames[role_id];
+        } else {
+          return "";
+        }
+      }
+    },
+    {
+      title: "操作",
+      render: user => (
+        <span>
+          <LinkButton onClick={() => showUpdate(user)}>修改</LinkButton>
+          <LinkButton onClick={() => deleteUser(user)}>删除</LinkButton>
+        </span>
+      )
+    }
+  ]);
+
+   //获取用户列表
+   const getUsers = async () => {
+    const result = await reqUsers();
+    if (result.status === 0) {
+      const { user, roles } = result.data;
+      initRoleNames(roles);
+      // this.setState({ users: user, roles });
+      setUsers(user)
+      setRoles(roles)
+    }
   };
 
-  initColumns = () => {
-    this.columns = [
-      {
-        title: "用户名",
-        dataIndex: "username"
-      },
-      {
-        title: "邮箱",
-        dataIndex: "email"
-      },
-      {
-        title: "电话",
-        dataIndex: "phone"
-      },
-      {
-        title: "创建时间",
-        dataIndex: "create_time",
-        render: create_time => formateDate(create_time)
-      },
-      {
-        title: "所属角色",
-        dataIndex: "role_id",
-        render: role_id => {
-          if (role_id) {
-            return this.roleNames[role_id];
-          } else {
-            return "";
-          }
-        }
-      },
-      {
-        title: "操作",
-        render: user => (
-          <span>
-            <LinkButton onClick={() => this.showUpdate(user)}>修改</LinkButton>
-            <LinkButton onClick={() => this.deleteUser(user)}>删除</LinkButton>
-          </span>
-        )
-      }
-    ];
-  };
+  //用户列表相关
+  useEffect(() => {
+    getUsers();
+    console.log("getUsers()");
+  },[roleNames]);
 
   //更改请求到的角色数据，以便表格处理
-  initRoleNames = roles => {
+  const initRoleNames = roles => {
     const roleNames = roles.reduce((pre, role) => {
       pre[role._id] = role.name;
       return pre;
     }, {});
     //保存
-    this.roleNames = roleNames;
+    // this.roleNames = roleNames;
+    setRoleNames(roleNames)
   };
 
-  showAdd=()=>{
-    this.user={}
+  const showAdd = () => {
+    this.user = {};
     this.setState({
-      isShow:true
-    })
-  }
+      isShow: true
+    });
+  };
 
   //添加/更新用户
-  addOrUpdateUser = async () => {
+  const addOrUpdateUser = async () => {
     // console.log(this.form);
     const user = this.form.reduce((pre, index) => {
       pre[index.name[0]] = index.value;
@@ -96,24 +113,15 @@ class User extends Component {
   };
 
   //修改界面
-  showUpdate = user => {
+  const showUpdate = user => {
     this.user = user; //保存user
     this.setState({ isShow: true });
   };
 
-  //获取用户列表
-  getUsers = async () => {
-    const result = await reqUsers();
-    // console.log(result);
-    if (result.status === 0) {
-      const { user, roles } = result.data;
-      this.initRoleNames(roles);
-      this.setState({ users: user, roles });
-    }
-  };
+ 
 
   //删除用户
-  deleteUser = user => {
+  const deleteUser = user => {
     if (user.username === storageUtils.getUser().username) {
       message.error("住手自己人！！！");
       return;
@@ -125,7 +133,6 @@ class User extends Component {
         console.log(result);
         if (result.status === 0) {
           message.success("删除成功");
-
           this.getUsers();
         } else {
           message.error("删除失败");
@@ -134,49 +141,39 @@ class User extends Component {
     });
   };
 
-  componentWillMount() {
-    this.initColumns();
-  }
-
-  componentDidMount() {
-    this.getUsers();
-  }
-
-  render() {
-    const title = (
-      <Button type="primary" onClick={this.showAdd}>
-        创建用户
-      </Button>
-    );
-    const { users, roles, isShow } = this.state;
-    const user = this.user||{}; //看当前环境下有没有user
-    return (
-      <Card title={title}>
-        <Table
-          bordered
-          rowKey="_id"
-          dataSource={users}
-          columns={this.columns}
-          pagination={{ defaultPageSize: PAGE_SIZE }}
+  const title = (
+    <Button type="primary" onClick={showAdd}>
+      创建用户
+    </Button>
+  );
+  // const { users, roles, isShow } = this.state;
+  // const user = this.user || {}; //看当前环境下有没有user
+  return (
+    <Card title={title}>
+      <Table
+        bordered
+        rowKey="_id"
+        dataSource={users}
+        columns={columns}
+        pagination={{ defaultPageSize: PAGE_SIZE }}
+      />
+      <Modal
+        title={user._id ? "修改用户" : "添加用户"}
+        visible={isShow}
+        onOk={addOrUpdateUser}
+        onCancel={() => this.setState({ isShow: false })}
+        destroyOnClose={true}
+      >
+        <UserForm
+          roles={roles}
+          user={user}
+          onChange={newFields => {
+            this.form = newFields;
+          }}
         />
-        <Modal
-          title={user._id ? "修改用户" : "添加用户"}
-          visible={isShow}
-          onOk={this.addOrUpdateUser}
-          onCancel={() => this.setState({ isShow: false })}
-          destroyOnClose={true}
-        >
-          <UserForm
-            roles={roles}
-            user={user}
-            onChange={newFields => {
-              this.form = newFields;
-            }}
-          />
-        </Modal>
-      </Card>
-    );
-  }
+      </Modal>
+    </Card>
+  );
 }
 
 export default User;
